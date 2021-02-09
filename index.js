46e6e9750cd52e2d154350d68ca9fd31d56102c6
Johan Bernt <76895511+JohanBernt@users.noreply.github.com>
2021-02-09 21:12:32 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+
+if (count % 3 === 0 && count % 5 === 0) {
+  output.push("FizzBuzz"); //if divisible by 3 and 5 then push FizZBuzz.
+}
+
+else if (count % 3 === 0) {
+  output.push("Fizz"); //if divisible by 3 then push Fizz.
+  } 
+  else if (count % 5 === 0) { //if divisible by 5 then push Buzz.
+    output.push("Buzz");
+  } 
+  else {
+    output.push(count);
+  }
+
+count++; // adds plus one to the extra number that was added to the array.
+
+  console.log(output);
+}
+
+
